7689e1a2d04d53237ea9c822376a6fc7cb2ea011
defscarr <anon.aryan.admin@protonmail.com>
2023-05-21 02:00:27 +0000

    chore(tailwindcss): Update tailwind.config.js with design system from figma file
    	* Add custom colors to tailwind css classes
    	* Add custom font sizes to tailwind css classes
    	* Extend tailwind classes with custom letter spacing
    	* Extend tailwind classes with user defined line height

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,55 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '13x': '0.8125rem',
+        '14x': '0.875rem',
+        '15x': '0.9375rem',
+        '16x': '1rem',
+        '18x': '1.125rem',
+        '20x': '1.25rem',
+        '24x': '1.5rem',
+      },
+      lineHeight: {
+        'leading-19': '1.1875rem',
+        'leading-20': '1.25rem',
+        'leading-22': '1.375rem',
+        'leading-23': '1.4375rem',
+        'leading-26': '1.625rem',
+        'leading-29': '1.8125rem',
+        'leading-35': '2.1875rem',
+
+      },
+      letterSpacing: {
+        'tracking-tight':'-0.0125em',
+        'tracking-tighter':'-0.015625em',
+        'tracking-tightest':'-0.020625em',
+      },
+      colors: {
+        'fuchisia-bold': '#AD1FEA',
+        'indigo-blue': '#4661E6',
+        'slate-bolder': '#373F68',
+        'violet-white': '#F2F4FF',
+        'slate-white': '#F7F8FD',
+        'slate-bold': '#3A4374',
+        'slate-gray':'#647196',
+        'orange-fade': '#F49F85',
+        'cyan-fade': '#62BCFA',
+        'red-bold': '#D73737',
+        
+
+
+
+
+
+
+
+
+
+      },
+
+    },
   },
   plugins: [],
 }
