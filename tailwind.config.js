a296184b0be428d440c01b8047729f4f281214c8
defscarr <anon.aryan.admin@protonmail.com>
2023-05-22 02:12:11 +0000

    refactor: reformat custom styles in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,31 +6,36 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      fontSize: {
-        '13x': '0.8125rem',
-        '14x': '0.875rem',
-        '15x': '0.9375rem',
-        '16x': '1rem',
-        '18x': '1.125rem',
-        '20x': '1.25rem',
-        '24x': '1.5rem',
-      },
-      lineHeight: {
-        'leading-19': '1.1875rem',
-        'leading-20': '1.25rem',
-        'leading-22': '1.375rem',
-        'leading-23': '1.4375rem',
-        'leading-26': '1.625rem',
-        'leading-29': '1.8125rem',
-        'leading-35': '2.1875rem',
 
-      },
-      letterSpacing: {
-        'tracking-tight':'-0.0125em',
-        'tracking-tighter':'-0.015625em',
-        'tracking-tightest':'-0.020625em',
-      },
+    letterSpacing: {
+      'close':'-0.0125em',
+      'closer':'-0.015625em',
+      'closest':'-0.020625em',
+    },
+    lineHeight: {
+      '19': '1.1875rem',
+      '20': '1.25rem',
+      '22': '1.375rem',
+      '23': '1.4375rem',
+      '26': '1.625rem',
+      '29': '1.8125rem',
+      '35': '2.1875rem',
+
+    },
+    fontSize: {
+      '13x': '0.8125rem',
+      '14x': '0.875rem',
+      '15x': '0.9375rem',
+      '16x': '1rem',
+      '18x': '1.125rem',
+      '20x': '1.25rem',
+      '24x': '1.5rem',
+    },
+
+    extend: {
+
+
+
       colors: {
         'fuchisia-bold': '#AD1FEA',
         'indigo-blue': '#4661E6',
@@ -42,14 +47,6 @@ module.exports = {
         'orange-fade': '#F49F85',
         'cyan-fade': '#62BCFA',
         'red-bold': '#D73737',
-        
-
-
-
-
-
-
-
 
 
       },
